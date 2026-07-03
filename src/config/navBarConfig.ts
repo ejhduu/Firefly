b86acc1d568bd74aac14f5dcdeb9c77af20b2d7f
ejhduu <ejhduu@163.com>
2026-07-03 04:14:07 +0000

    feat: 克隆并运行yzttz项目
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -90,7 +90,7 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 				icon: "fa7-brands:gitee",
 			},
 			{
-				name: "QQ交流群",
+				name: "原模版交流群",
 				url: "https://qm.qq.com/q/ZGsFa8qX2G",
 				external: true,
 				icon: "fa7-brands:qq",
